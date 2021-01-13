f0eb331e8f1755b0072ee010ce4ef350c1a30c54
COM <COM@DESKTOP-L2F7LSG>
2021-01-13 16:36:25 +0900

    주석 추가

--- src/main/java/com/mycompany/webapp/controller/testController.java
+++ src/main/java/com/mycompany/webapp/controller/testController.java
@@ -1,5 +1,13 @@
 package com.mycompany.webapp.controller;
 
-public class testController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/*@Controller
+@RequestMapping("/test")*/
+public class testController {
+	//field
+	//constructor
+	//method
+	
 }
